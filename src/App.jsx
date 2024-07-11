0597de1e9655adc84de932f6f3c322ea882137ab
Etinofficial01 <emmanuelosemwengie@gmail.com>
2024-07-11 19:26:56 +0100

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Search from "./components/Search";
 import NavBar from "./components/navbar";
 import Body from "./components/Body";
-import Hero from "./components/Hero";
 import FindHeader from "./components/findJob/findHeader";
 import FindBody from "./components/findJob/findBody";
 
@@ -11,7 +10,6 @@ function App() {
       <NavBar />
       <Search />
       <Body />
-      <Hero />
       <FindHeader />
       <FindBody />
     </div>
