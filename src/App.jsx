7f6590d0e965994203b804d5173d86116ce9531c
Raphael <adebayoraphael3@gmail.com>
2024-07-12 22:52:20 +0700

    Complete Login and Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import Login from "./component/login.jsx";
+import Signup from "./component/signup.jsx";
 function App() {
+  const [currentForm, setCurrentForm] = useState("login")
+  const toggleForm = (formName)=>{
+    setCurrentForm(formName);
+  }
   return (
     <>
-      <h1 className="text-red-500">Hello Live Project</h1>
+    {
+        currentForm === "login"?<Login onFormSwitch={toggleForm} />:<Signup onFormSwitch={toggleForm}/>
+    }
+    
     </>
   );
 }
